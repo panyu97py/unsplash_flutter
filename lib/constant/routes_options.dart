524ae93f4d14c8ea13a260d1e60f1b868fbf5e1b
yu.pan <panyu@pinpianyi.com>
2022-02-21 10:59:13 +0800

    代码优化

--- lib/constant/routes_options.dart
+++ lib/constant/routes_options.dart
@@ -1,13 +1,13 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:unsplash_flutter/utils/no_animation_page_route.dart';
 import 'package:unsplash_flutter/views/collections/collections_view.dart';
 import 'package:unsplash_flutter/views/home/home_view.dart';
 import 'package:unsplash_flutter/views/login/login_view.dart';
 import 'package:unsplash_flutter/views/my/my_view.dart';
 import 'package:unsplash_flutter/views/photoDetail/photo_detail_view.dart';
-import 'package:unsplash_flutter/main.dart';
+import 'package:unsplash_flutter/layouts/main_layout.dart';
 
 class PageName {
-
   static const String drawer = "drawer";
 
   static const String my = 'my';
@@ -21,27 +21,54 @@ class PageName {
   static const String photoDetail = 'photoDetail';
 }
 
-class PageRouters {
+class PageRoutersOptions {
 
+  /// 初始化的路由
   static const String initRoute = PageName.home;
 
+  /// Drawer/二级 路由配置
   static final Map<String, WidgetBuilder> drawerRoutes = {
-
     PageName.my: (BuildContext context) => const MyView(),
-
     PageName.home: (BuildContext context) => const HomeView(),
-
     PageName.collections: (BuildContext context) => const CollectionsView(),
-
   };
 
+  /// 普通/顶层 路由配置
   static final Map<String, WidgetBuilder> normalRoutes = {
-
     PageName.login: (BuildContext context) => const LoginView(),
-
     PageName.photoDetail: (BuildContext context) => const PhotoDetailView(),
-
-    PageName.drawer: (BuildContext context) => const HomeDrawerView()
-
   };
+
+  /// 构建 Drawer/二级 路由布局
+  static Widget _buildDrawerRouteLayout(BuildContext context) {
+    return const MainLayout(child: Navigator(initialRoute: initRoute, onGenerateRoute: generateDrawerRoute));
+  }
+
+  /// 生成 普通/顶层 路由
+  static Route<dynamic>? generateNormalRoute(RouteSettings settings) {
+
+    bool isNormalRoute = normalRoutes.containsKey(settings.name);
+
+    bool isDrawerRoute = drawerRoutes.containsKey(settings.name);
+
+    if (isDrawerRoute) {
+      return MaterialPageRoute(builder: _buildDrawerRouteLayout, settings: settings);
+    }
+
+    if (isNormalRoute) {
+      return MaterialPageRoute(builder: normalRoutes[settings.name]!, settings: settings);
+    }
+
+    throw Exception('Invalid normal route: ${settings.name}');
+  }
+
+  ///  生成 Drawer/二级 路由
+  static Route<dynamic> generateDrawerRoute(RouteSettings settings) {
+
+    if (drawerRoutes.containsKey(settings.name)) {
+      return NoAnimationPageRoute(builder: drawerRoutes[settings.name]!);
+    }
+
+    throw Exception('Invalid drawer route: ${settings.name}');
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:unsplash_flutter/layouts/main_layout.dart';
 import 'package:unsplash_flutter/constant/routes_options.dart';
-import 'package:unsplash_flutter/utils/no_animation_page_route.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,37 +14,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.green,
         ),
-        initialRoute: PageRouters.initRoute,
-        onGenerateRoute: generateNormalRoute);
-  }
-
-  Route<dynamic>? generateNormalRoute(RouteSettings settings) {
-    bool isNormalRoute = PageRouters.normalRoutes.containsKey(settings.name);
-    bool isDrawerRoute = PageRouters.drawerRoutes.containsKey(settings.name);
-    if (!isNormalRoute && !isDrawerRoute) {
-      throw Exception('Invalid normal route: ${settings.name}');
-    }
-    if (isDrawerRoute) {
-      return MaterialPageRoute(builder: PageRouters.normalRoutes[PageName.drawer]!,settings: settings);
-    }
-    WidgetBuilder? builder = PageRouters.normalRoutes[settings.name];
-    return MaterialPageRoute(builder: builder!,settings: settings);
-  }
-}
-
-class HomeDrawerView extends StatelessWidget {
-  const HomeDrawerView({Key? key}) : super(key: key);
-
-  Route<dynamic> generateDrawerRoute(RouteSettings settings) {
-    if (!PageRouters.drawerRoutes.containsKey(settings.name)) {
-      throw Exception('Invalid drawer route: ${settings.name}');
-    }
-    WidgetBuilder? builder = PageRouters.drawerRoutes[settings.name];
-    return NoAnimationPageRoute(builder: builder!);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MainLayout(child: Navigator(initialRoute: PageRouters.initRoute, onGenerateRoute: generateDrawerRoute));
+        initialRoute: PageRoutersOptions.initRoute,
+        onGenerateRoute: PageRoutersOptions.generateNormalRoute);
   }
 }
